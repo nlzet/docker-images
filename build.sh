d395de991f486921325d3ff2d15300ebc0a55b61
Niels V <niels@3wmedia.nl>
2019-10-10 11:10:12 +0200

    setup php 7.4 rc

--- build.sh
+++ build.sh
@@ -7,14 +7,20 @@ CACHED=
 docker rmi nlzet/php56-cli --force && \
 docker rmi nlzet/php56-fpm --force && \
 docker rmi nlzet/php73-cli --force && \
-docker rmi nlzet/php73-fpm --force
+docker rmi nlzet/php73-fpm --force && \
+docker rmi nlzet/php74-cli --force && \
+docker rmi nlzet/php74-fpm --force
 
 # Build and push
-docker build --pull -t nlzet/php56-cli php/php56-cli && \
+docker build --pull -t nlzet/php56-cli php/php56-cli ${CACHED} && \
 docker push nlzet/php56-cli && \
 docker build --pull -t nlzet/php56-fpm php/php56-fpm ${CACHED} && \
 docker push nlzet/php56-fpm && \
-docker build --pull -t nlzet/php73-cli php/php73-cli ${CACHED}
+docker build --pull -t nlzet/php73-cli php/php73-cli ${CACHED} && \
 docker push nlzet/php73-cli && \
 docker build --pull -t nlzet/php73-fpm php/php73-fpm ${CACHED} && \
-docker push nlzet/php73-fpm
+docker push nlzet/php73-fpm && \
+docker build --pull -t nlzet/php74-cli php/php74-cli ${CACHED} && \
+docker push nlzet/php74-cli && \
+docker build --pull -t nlzet/php74-fpm php/php74-fpm ${CACHED} && \
+docker push nlzet/php74-fpm
