4254ae5c916d75bfa588a9d747f2c080a81e9fc1
Niels Verbeek <niels@kreable.com>
2020-03-28 16:27:55 +0100

    ci images build

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-#CACHED=--no-cache
-CACHED=
+CACHED=--no-cache
+#CACHED=
 
 # cleanup existing images
 docker rmi nlzet/php56-cli --force && \
 docker rmi nlzet/php56-fpm --force && \
 docker rmi nlzet/php73-cli --force && \
 docker rmi nlzet/php73-fpm --force && \
+docker rmi nlzet/php73-ci --force && \
 docker rmi nlzet/php74-cli --force && \
-docker rmi nlzet/php74-fpm --force
+docker rmi nlzet/php74-fpm --force && \
+docker rmi nlzet/php74-ci --force
 
 # Build and push
 docker build --pull -t nlzet/php56-cli php/php56-cli ${CACHED} && \
@@ -20,7 +22,11 @@ docker build --pull -t nlzet/php73-cli php/php73-cli ${CACHED} && \
 docker push nlzet/php73-cli && \
 docker build --pull -t nlzet/php73-fpm php/php73-fpm ${CACHED} && \
 docker push nlzet/php73-fpm && \
+docker build --pull -t nlzet/php73-ci php/php73-ci ${CACHED} && \
+docker push nlzet/php73-ci && \
 docker build --pull -t nlzet/php74-cli php/php74-cli ${CACHED} && \
 docker push nlzet/php74-cli && \
 docker build --pull -t nlzet/php74-fpm php/php74-fpm ${CACHED} && \
 docker push nlzet/php74-fpm
+docker build --pull -t nlzet/php74-ci php/php74-ci ${CACHED} && \
+docker push nlzet/php74-ci
