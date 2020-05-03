c85263a6d8110097ab96e4722d37dde568fed0d0
Niels Verbeek <niels@3wmedia.nl>
2020-05-03 17:13:14 +0200

    fix php extension volume dir + cleanup

--- build.sh
+++ build.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
 CACHED=--no-cache
-#CACHED=
+# CACHED=
 
-# cleanup existing images
-#docker rmi nlzet/php56-cli --force && \
-#docker rmi nlzet/php56-fpm --force && \
-docker rmi nlzet/php73-cli --force
+docker rmi nlzet/php56-cli --force && \
+docker rmi nlzet/php56-fpm --force && \
+docker rmi nlzet/php73-cli --force && \
 docker rmi nlzet/php73-fpm --force && \
 docker rmi nlzet/php73-ci --force && \
 docker rmi nlzet/php74-cli --force && \
 docker rmi nlzet/php74-fpm --force && \
-docker rmi nlzet/php74-ci --force
+docker rmi nlzet/php74-ci --force && \
 
-# Build and push
-#docker build --pull -t nlzet/php56-cli php/php56-cli ${CACHED} && \
-#docker push nlzet/php56-cli && \
-#docker build --pull -t nlzet/php56-fpm php/php56-fpm ${CACHED} && \
-#docker push nlzet/php56-fpm && \
-#docker build --pull -t nlzet/php73-cli php/php73-cli ${CACHED}
+docker build --pull -t nlzet/php56-cli php/php56-cli ${CACHED} && \
+docker push nlzet/php56-cli && \
+docker build --pull -t nlzet/php56-fpm php/php56-fpm ${CACHED} && \
+docker push nlzet/php56-fpm && \
+docker build --pull -t nlzet/php73-cli php/php73-cli ${CACHED} && \
 docker push nlzet/php73-cli && \
 docker build --pull -t nlzet/php73-fpm php/php73-fpm ${CACHED} && \
 docker push nlzet/php73-fpm && \
