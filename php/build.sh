63279b4890bfcac80c6aa40f8afa43428080e844
Niels Verbeek <niels@3wmedia.nl>
2020-12-06 17:55:32 +0100

    debug ci

--- php/build.sh
+++ php/build.sh
@@ -7,7 +7,7 @@ build () {
   echo "> Building image: ${DOCKER_TAG}-$1"
   echo "#########"
   echo " "
-  echo docker build --build-arg PHP_EXTENSIONS="${PHP_EXTENSIONS}" --build-arg FROM_IMAGE=${FROM_IMAGE} --target $2 -t ${DOCKER_TAG}-$1 -t ${DOCKER_TAG_MINOR}-$1 php/ $3
+  echo docker build --build-arg PHP_EXTENSIONS=\"${PHP_EXTENSIONS}\" --build-arg FROM_IMAGE=${FROM_IMAGE} --target $2 -t ${DOCKER_TAG}-$1 -t ${DOCKER_TAG_MINOR}-$1 php/ $3
   docker build --build-arg PHP_EXTENSIONS="${PHP_EXTENSIONS}" --build-arg FROM_IMAGE=${FROM_IMAGE} --target $2 -t ${DOCKER_TAG}-$1 -t ${DOCKER_TAG_MINOR}-$1 php/ $3
 }
 
