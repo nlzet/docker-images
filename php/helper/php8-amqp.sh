870d3652fe76d5aa4528d01a9779b143032eea99
Niels Verbeek <niels@3wmedia.nl>
2020-12-19 23:13:52 +0100

    remove amqp multistage, does not support cross-compatible dockerfile

--- php/helper/php8-amqp.sh
+++ php/helper/php8-amqp.sh
@@ -2,21 +2,22 @@
 
 PHP_MAJOR_VERSION=$(echo $PHP_VERSION | cut -d "." -f1)
 
-# move and configure multistage build output
-configure_amqp8 () {
-  echo "> configuring php8 amqp build"
-  echo "extension=amqp.so" > /usr/local/etc/php/conf.d/docker-php-ext-amqp.ini
-  mv -v /usr/local/lib/php/extensions/no-debug-non-zts-20200930/amqp8.so /usr/local/lib/php/extensions/no-debug-non-zts-20200930/amqp.so
+install_amqp8 () {
+  echo "> installing php8 amqp source build"
+  docker-php-source extract
+  apt update && apt -y install --no-install-recommends git librabbitmq-dev \
+  git clone --branch master --depth 1 https://github.com/php-amqp/php-amqp.git /usr/src/php/ext/amqp \
+  cd /usr/src/php/ext/amqp && git submodule update --init \
+  docker-php-ext-install amqp
+  rm -rf /usr/src/php/ext/amqp /tmp/* /var/tmp/* /var/lib/{apt,dpkg,cache,log}/
 }
 
-# remove multistage build output, will be installed with pecl for PHP < 8
 skip_amqp8 () {
-  echo "> skipping php8 amqp build for PHP $PHP_MAJOR_VERSION"
-  rm -f /usr/local/lib/php/extensions/no-debug-non-zts-20200930/amqp8.so
+  echo "> skipping php8 amqp source build for PHP $PHP_MAJOR_VERSION"
 }
 
 if [ "$PHP_MAJOR_VERSION" -eq "8" ]; then
-  configure_amqp8
+  install_amqp8
 else
   skip_amqp8
 fi
